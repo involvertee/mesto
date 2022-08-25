b4ae56736cb5aedea4c5331277bb7bdd55d42d74
Anastasia <asays1@gmail.com>
2022-08-25 12:25:02 +0300

    1st iteration bug-fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close');
-let saveButton = document.querySelector('.popup__save-button');
-let form = document.querySelector('.popup__form');
-let formName = document.querySelector('.popup__input_type_name');
-let formInfo = document.querySelector('.popup__input_type_info');
-let nameInput = document.querySelector('.profile__title');
-let jobInput = document.querySelector('.profile__subtitle');
+let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__close");
+let saveButton = document.querySelector(".popup__save-button");
+let form = document.querySelector(".popup__form");
+let inputName = document.querySelector(".popup__input_type_name");
+let inputInfo = document.querySelector(".popup__input_type_info");
+let userName = document.querySelector(".profile__title");
+let userInfo = document.querySelector(".profile__subtitle");
 
-function editProfile () {
- popup.classList.add('popup_opened');
- formName.value = nameInput.textContent;
- formInfo.value = jobInput.textContent;
+function editProfile() {
+  popup.classList.add("popup_opened");
+  inputName.value = userName.textContent;
+  inputInfo.value = userInfo.textContent;
 }
-editButton.addEventListener('click', editProfile);
+editButton.addEventListener("click", editProfile);
 
-function closePopup () {
-    popup.classList.remove('popup_opened');
+function closePopup() {
+  popup.classList.remove("popup_opened");
 }
-closeButton.addEventListener('click', closePopup);
+closeButton.addEventListener("click", closePopup);
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    nameInput.textContent = formName.value;
-    jobInput.textContent = formInfo.value;
-    closePopup();
-    
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  userName.textContent = inputName.value;
+  userInfo.textContent = inputInfo.value;
+  closePopup();
 }
-saveButton.addEventListener('click', formSubmitHandler);
+form.addEventListener("submit", formSubmitHandler);
