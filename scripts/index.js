3687767b16b6bd620d6fcfc5cf6735a67b5cfa2c
Anastasia <asays1@gmail.com>
2022-08-30 21:51:57 +0300

    feat: adding cards, transitions on popups

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,40 @@
-let editButton = document.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let closeButton = document.querySelector(".popup__close");
-let saveButton = document.querySelector(".popup__save-button");
-let form = document.querySelector(".popup__form");
-let inputName = document.querySelector(".popup__input_type_name");
-let inputInfo = document.querySelector(".popup__input_type_info");
-let userName = document.querySelector(".profile__title");
-let userInfo = document.querySelector(".profile__subtitle");
+const editButtonInfo = document.querySelector(".profile__edit-button");
+const addButtonPlace = document.querySelector(".profile__add-button");
+const popupInfo = document.querySelector(".popup_info");
+const popupPlace = document.querySelector(".popup_place");
+const closeButtonInfo = document.querySelector(".popup__close");
+const closeButtonPlace = document.querySelector(".popup__close_place");
+const saveButton = document.querySelector(".popup__save-button");
+const formInfo = document.querySelector(".popup__form-info");
+const formPlace = document.querySelector(".popup__form-place");
+const inputName = document.querySelector(".popup__input_type_name");
+const inputInfo = document.querySelector(".popup__input_type_info");
+const inputPlaceName = document.querySelector(".popup__input_type_place-name");
+const inputPlaceLink = document.querySelector(".popup__input_type_place-link");
+const userName = document.querySelector(".profile__title");
+const userInfo = document.querySelector(".profile__subtitle");
 
-function editProfile() {
-  popup.classList.add("popup_opened");
-  inputName.value = userName.textContent;
-  inputInfo.value = userInfo.textContent;
+//функции
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
-editButton.addEventListener("click", editProfile);
-
-function closePopup() {
-  popup.classList.remove("popup_opened");
+function closePopup(popup){
+  popup.classList.remove('popup_opened');
 }
-closeButton.addEventListener("click", closePopup);
+const elementsList = document.querySelector(".elements__list");
+function makeNewElement(elname, ellink) {  //функция создания карточки
+  const elementTemplate = document.querySelector(".element__template").content;
+  // клонируем содержимое тега template
+  const newElement = elementTemplate
+    .querySelector(".elements__item")
+    .cloneNode(true);
+  // наполняем содержимым
+  newElement.querySelector(".elements__image").src = ellink;
+  newElement.querySelector(".elements__image").alt = elname;
+  newElement.querySelector(".elements__title").textContent = elname;
+  // отображаем на странице
+  return newElement;
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -26,27 +42,42 @@ function formSubmitHandler(evt) {
   userInfo.textContent = inputInfo.value;
   closePopup();
 }
-form.addEventListener("submit", formSubmitHandler);
 
-//ПР5
-
-function makeNewElement(elname, ellink) { //функция создания карточки
-  const elementTemplate = document.querySelector('.element__template').content; 
-  const elementsList = document.querySelector('.elements__list');
-  // клонируем содержимое тега template
-  const newElement = elementTemplate.querySelector('.elements__item').cloneNode(true);
-  // наполняем содержимым
-  newElement.querySelector('.elements__image').src = ellink;
-  newElement.querySelector('.elements__image').alt = elname;
-  newElement.querySelector('.elements__title').textContent = elname;
-  i++;
-  // отображаем на странице
-  elementsList.append(newElement); 
+//константы с вызовом функций
+const editProfile = function () {
+  inputName.value = userName.textContent;
+  inputInfo.value = userInfo.textContent;
+  openPopup(popupInfo);
+}
+const closePopupInfo = function(){
+  popupInfo.classList.remove('popup_opened');
+}
+const openPopupPlace = function () {
+  openPopup(popupPlace);
+}
+const closePopupPlace = function(){
+  popupPlace.classList.remove('popup_opened');
 }
 
+const addElement = function (evt){ //добавляем карточку сами
+  evt.preventDefault();
+  const elementCard = makeNewElement(inputPlaceName.value, inputPlaceLink.value);
+  elementsList.prepend(elementCard);
+  closePopup(popupPlace);
+}
 
-i=0;
+// метро люблино работаем
+editButtonInfo.addEventListener("click", editProfile); // открыть редактирование профиля
+closeButtonInfo.addEventListener("click", closePopupInfo); // закрыть редактирование профиля
+
+addButtonPlace.addEventListener("click", openPopupPlace); // открыть добавление места
+closeButtonPlace.addEventListener("click", closePopupPlace); // закрыть добавление места
+
+formInfo.addEventListener("submit", formSubmitHandler);
+formPlace.addEventListener('submit', addElement);
+
 // инициализируем все карточки
 initialCards.forEach((el) => {
-  makeNewElement(el.name, el.link); 
+  newElement = makeNewElement(el.name, el.link);
+  elementsList.append(newElement);
 });
